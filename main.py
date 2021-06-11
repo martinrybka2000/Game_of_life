0c3bfa410071f83837ec7b2a38f1cf1d3c012f07
martinrybka2000 <rybatinek14@gmail.com>
2021-06-11 18:39:28 +0200

    com

--- main.py
+++ main.py
@@ -8,6 +8,18 @@ pygame.display.set_caption("Moja Gra")
 
 run = True
 
+cnt = 0
+
+
+class Grid:
+    def __init__(self):
+        win.fill((255, 255, 255))
+        pygame.draw.line(win, (0, 255, 0), (-50, 0), (200, 200), 10)
+        pygame.display.update()
+
+
+x = Grid()
+
 while run:
     # obsługa zdarzeń
     for event in pygame.event.get():
@@ -18,5 +30,14 @@ while run:
     # warunki do zmiany pozycji obiektu
     if keys[pygame.K_ESCAPE]:
         run = False
+
+    elif keys[pygame.K_LEFT]:
+        win.scroll(1, 1)
+        print(str(win.get_abs_offset()))
+        win.fill((255, 255, 255))
+        cnt += 10
+        pygame.draw.line(win, (0, 255, 0), (-50 + cnt, 0),
+                         (200 + cnt, 200), 10)
+        pygame.display.update()
 
     pygame.time.delay(50)
